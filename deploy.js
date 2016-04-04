8fc2b2468531d6b489a3d6c504102ed298ec0c65
Copons <Copons@users.noreply.github.com>
2016-04-04 13:56:41 +0200

    Changed webpack production config file name

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   const childProcess = require('child_process');
   childProcess.exec(
-    'webpack -p --config webpack.production.config.js',
+    'webpack -p --config webpack.config.production.js',
     (error, stdout, stderr) => {
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
