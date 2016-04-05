578fa4110fc32618f72ae7852306688cc622a303
Copons <Copons@users.noreply.github.com>
2016-04-05 16:22:38 +0200

    Basic game info bar

--- app/components/game/Game.js
+++ app/components/game/Game.js
@@ -1,8 +1,10 @@
+import './game.css';
 //import { RULES } from '../../config';
 //import { random } from '../../libs/math';
+import { qs, addClass, removeClass } from '../../libs/dom';
 import IO from '../communications/IO';
 import Message from '../communications/Message';
-//import PubSub from '../communications/PubSub';
+import PubSub from '../communications/PubSub';
 //import Player from '../player/Player';
 
 
@@ -10,22 +12,60 @@ export default class Game {
 
   constructor() {
     this.room = {};
+    this.turn = 0;
+
+    this.el = {
+      game: document.getElementById('game'),
+      info: document.getElementById('info'),
+    };
+    this.el.turn = qs('.turn .count', this.el.info);
+    this.el.players = qs('.players', this.el.info);
 
     IO.io.on('Game.start', this.start);
     IO.io.on('Game.closed', this.close);
+    PubSub.sub('Menu.leaveRoom', this.close);
   }
 
 
   start = response => {
-    console.log(response);
     this.room = response.body.room;
+    this.turn = 0;
+    this.activePlayer = response.body.activePlayer;
+    this.render();
   }
 
 
   close = response => {
     console.log(response);
+    addClass(this.el.game, 'hidden');
     this.room = {};
+    this.turn = 0;
     Message.error(response.message);
+  }
+
+
+  render = () => {
+    this.el.turn.textContent = this.turn;
+    for (const player of this.room.players) {
+      this.el.players.insertAdjacentHTML('beforeend', `
+        <div class="player">
+          <div class="name">${player.name}</div>
+          <div class="pieces">
+            <span class="count"></span>
+            <span class="icon"></span>
+          </div>
+          <div class="cards">
+            <span class="count"></span>
+            <span class="icon"></span>
+          </div>
+          <div class="destinations">
+            <span class="count"></span>
+            <span class="icon"></span>
+          </div>
+        </div>
+      `);
+    }
+    removeClass(this.el.game, 'hidden');
   }
 
 }
