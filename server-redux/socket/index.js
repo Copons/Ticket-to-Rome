ed6249d655086587093d2e30ebdf0b994db16d62
Copons <Copons@users.noreply.github.com>
2016-05-12 12:08:04 +0200

    Streamlined promises in Socket.io listeners

--- server-redux/socket/index.js
+++ server-redux/socket/index.js
@@ -5,76 +5,68 @@ import Players from '../services/Players';
 import Rooms from '../services/Rooms';
 import store from '../store';
 
+
 export default function socket(server) {
   const io = socketIo.listen(server);
 
-  /*const unsubscribe = */
   store.subscribe(() => {
     //console.log(JSON.stringify(store.getState()));
   });
 
-  //unsubscribe();
-
   io.on(API.CONNECTION, client => {
     console.log(`Client ${client.id} connected.`);
 
-    client.on(API.CREATE_PLAYER, (player, callback) => {
+
+
+
+    client.on(API.CREATE_PLAYER, (player, callback) =>
       Players.create(player)
-        .then(res => {
-          callback(res);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+    );
 
-    client.on(API.UPDATE_PLAYER, (player, callback) => {
+    client.on(API.UPDATE_PLAYER, (player, callback) =>
       Players.update(player)
-        .then(res => {
-          Rooms.emitAll(io.sockets);
-          callback(res);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+    );
 
-    client.on(API.CREATE_ROOM, (room, callback) => {
+
+
+
+    client.on(API.CREATE_ROOM, (room, callback) =>
       Rooms.create(room, client)
-        .then(res => {
-          Rooms.emitAll(io.sockets);
-          callback(res);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+    );
 
-    client.on(API.JOIN_ROOM, (data, callback) => {
+    client.on(API.JOIN_ROOM, (data, callback) =>
       Rooms.join(data.roomId, data.playerId, client)
-        .then(res => {
-          Rooms.emitAll(io.sockets);
-          callback(res);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+    );
 
-    client.on(API.LEAVE_ROOM, (data, callback) => {
+    client.on(API.LEAVE_ROOM, (data, callback) =>
       Rooms.leave(data.roomId, data.playerId, client)
-        .then(res => {
-          Rooms.emitAll(io.sockets);
-          callback(res);
-        })
-        .catch(err => {
-          callback(err);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+        .catch(err => callback(err))
+    );
 
-    client.on(API.START_GAME, (roomId, callback) => {
+    client.on(API.START_GAME, (roomId, callback) =>
       Games.start(roomId)
-        .then(res => {
-          Rooms.emitAll(io.sockets);
-          callback(res);
-        })
-        .catch(err => {
-          callback(err);
-        });
-    });
+        .then(res => callback(res))
+        .then(() => Rooms.emitAll(io.sockets))
+        .catch(err => callback(err))
+    );
+
+
+
 
     client.on(API.DISCONNECT, () => {
       Rooms.leaveAll(client)
         .then(clientId => Players.delete(clientId))
-        .then(() => Rooms.emitAll(io))
+        .then(() => Rooms.emitAll(io.sockets))
         .catch(err => {
           console.log(err);
         });
